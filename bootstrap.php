973713969d2363a0cc2d2db1d6db4b0670fc0c73
Martin Skorupa <mskorupa@pixelfederation.com>
2018-01-16 17:33:15 +0100

    bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -7,19 +7,9 @@ declare(strict_types=1);
  * @license:  Internal use only
  */
 
-use Flarum\Event\ConfigureLocales;
 use Flarum\Foundation\Application;
 use Illuminate\Contracts\Events\Dispatcher;
-use PixelFederation\LoginServer\Listener;
-use PixelFederation\LoginServer\Providers\JmsSerializerProvider;
 
 return function (Dispatcher $events, Application $app) {
-    $events->subscribe(Listener\AddClientAssets::class);
-    $events->subscribe(Listener\AddLoginServerAuthRoute::class);
-    $events->subscribe(Listener\AddRegistrationListener::class);
-    $events->subscribe(Listener\AddSsoCookieListener::class);
-    $events->listen(ConfigureLocales::class, function (ConfigureLocales $event) {
-        $event->loadLanguagePackFrom(__DIR__);
-    });
-    $app->register(JmsSerializerProvider::class);
+
 };
